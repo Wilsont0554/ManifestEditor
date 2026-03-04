fe6793287a164fc7a57bcad730bb366ea2fa728a
Jacob Way <jacobbway1997@gmail.com>
2026-03-04 11:41:23 -0600

    Update to container.js
    Corrected my getters and setters. Added the functions to change the fields within the container, when the type changes. I do not have the logic to call those function in place yet though.

--- src/ManifestClasses/Container.js
+++ src/ManifestClasses/Container.js
@@ -7,18 +7,27 @@ class Container{
     constructor(id, type){
         // Going to need better validation that the input here is a URL
         this.id = id == undefined ? 'https://example.org/manifest_Example.com' : id;
-        
         this.type = Container.VALID_TYPES.has(type) ? type : 'Default Type';
-        
-        if(this.type === 'Timeline') this.duration;
-        else if(this.type == 'Canvas') {
-            this.width;
-            this.height;
-        }
-
         this.items = []; // this would/will contain any of the optional json objects
-
+        this.addAnnotationPage(new AnnotationPage())
     }
+
+/*------------------------------------------------------------
+    These delete fields if the user changes manifest types
+--------------------------------------------------------------*/
+
+    deleteDimensions(){
+        this.height = null;
+        this.width = null;
+    }
+
+    deleteDuration(){
+        this.duration = null;
+    }
+
+/*------------------------------------------------------------
+                          GETTERS
+--------------------------------------------------------------*/   
 
     getItems(){
         return this.items();
@@ -36,13 +45,40 @@ class Container{
         return this.duration;
     }
 
-    getWidth(){
-        return this.width;
+    getDimensions(){
+        return [this.height, this.width];
     }
 
-    getHeight(){
-        return this.height;
+    getAnnotationPage(index){
+        if(index == undefined) index = 0;
+        return this.items[index];
     }
 
+/*------------------------------------------------------------
+                          SETTERS
+--------------------------------------------------------------*/
+    // This also will server as creating
+    // the duration variable
+    setDuration(duration){
+        this.duration = duration;
+    }
+    // This also will server as creating
+    // the dimesions
+    setDimensions(height, width){
+        this.height = height;
+        this.width = width;
+    }
+
+    setType(type){
+        this.type = type;
+    }
+
+/*------------------------------------------------------------
+                Something else
+--------------------------------------------------------------*/ 
+
+     addAnnotationPage(annotation){
+        this.items.push(annotation);
+     }
     
 } export default Container
