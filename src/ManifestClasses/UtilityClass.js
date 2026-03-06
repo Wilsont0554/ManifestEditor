bbbfd358f42d0245b63023c6bab418b233f02624
Jacob Way <jacobbway1997@gmail.com>
2026-03-06 10:55:19 -0600

    created utilityclass.js

---
+++ src/ManifestClasses/UtilityClass.js
@@ -0,0 +1,14 @@
+import Container from './Container.js';
+import Annotation from './ContentResource.js';
+import Label from './Label.js';
+
+class UtilityClass{
+	this.type = "Example/Blank";
+	this.id = "shouldBeA_URL";
+
+	constructor(type){
+		this.type = type;
+		this.id = id;
+
+	}
+} export default UtilityClass
