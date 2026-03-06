1b6010690b9577fc9d37a749629d9c16ed22e9b5
Jacob Way <jacobbway1997@gmail.com>
2026-03-06 10:19:04 -0600

    Merge branch 'main' into Container_Branch

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/ManifestEditor",
   define: {
     // Some dependencies expect a Node-style global object in the browser.
     global: "globalThis",
