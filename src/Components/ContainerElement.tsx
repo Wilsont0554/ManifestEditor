bf5eaea43740e751d0312020786c7b2972fc5598
Jacob Way <jacobbway1997@gmail.com>
2026-03-13 10:18:01 -0500

    Updating ContainerElement.jsx

--- src/Components/ContainerElement.tsx
+++ src/Components/ContainerElement.tsx
@@ -3,10 +3,34 @@ import Container from "./ManifestClasses/TypeScript/Container.ts";
 
 function ContainerElement(props){
 
-    const { }
+    const [type, setType] = useState("");
+    const { container, setcount, count } = props;
 
-    return(<>
+    const containerObj = container
+
+    return(
+    <>
+        <div className="sidebar-editor-container">
+            <div className="field-group">
+                <label>Type</label>
+                <select
+                    value={containerObj.getType() || ""}
+                    onChange={(e) => {
+                        containerObj.setType(e.target.value);
+                        setcount(count + 1);
+                    }}
+                >
+                    <option value="scene">Scene</option>
+                    <option value="timeline">Time Line</option>
+                    <option value="canvas">Canvas</option>
+                </select>
+            </div>
+
+            <div className="field-group">
+                <label></label>
+            </div>
+        </div>
         
     </>);
 
-} export default Container
+} export default ContainerElement;
