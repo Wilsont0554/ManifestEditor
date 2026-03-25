977c52352dab7f20133289f2106d0f7141a790d1
Jacob Way <jacobbway1997@gmail.com>
2026-03-25 11:54:20 -0500

    updated camera.ts
    -removed lets
    -removed constructor from abstract camera class
    -created getters/setters for all variables in camera class
    -created constructor for orthographic camera
    -created getters/setters for orthographic camera

--- src/ManifestClasses/TypeScript/SceneComponets/Camera.ts
+++ src/ManifestClasses/TypeScript/SceneComponets/Camera.ts
@@ -1,46 +1,100 @@
-import Label from '../ManifestClasses/TypeScript/Label.ts'
+import Label from '../Label'
 
 
 abstract class Camera{
-	let type: string;
-	let id?: string;
-	let label?: Label;
-	let lookAt?: LookAt;
-	let near?: number;
-	let far?: number;
+	type: string = "Default_Value";
+	id?: string;
+	label?: Label;
+	// lookAt?: LookAt;
+	near?: number;
+	far?: number;
 
-	public constructor(type: string, id?: string, near?: number, far?: number){
-		this.type = type;
-		id == undefined ? this.id = "Default" : this.id = id;
-		near == undefined ? this.near = 0.0 : this.near = near;
-		far == undefined ? this.far = 0.0 : this.far = far;
-	}
+/*------------------------------------------------------------
+    						ADD/CREATE
+--------------------------------------------------------------*/
 
 	addLabel(value: string, languageCode: string = 'en'): void{
 		this.label = new Label(value, languageCode);
 	}
 
+/*------------------------------------------------------------
+    						DELETE
+--------------------------------------------------------------*/
+
 	deleteLabel(): void{
-		delete this.Label;
+		delete this.label;
 	}
-
-}
-
-class OrthographicCamera extends Camera{
-	Camera.type = "OrthographicCamera";
-	let viewHeight: number = 0.0;
-
-	public constructor(type: string, id?: string, near?: number, far?: number, viewHeight: number){
+/*------------------------------------------------------------
+    						SETTERS
+--------------------------------------------------------------*/
+	setType(type: string){
 		this.type = type;
-		id == undefined ? this.id = "Default" : this.id = id;
-		near == undefined ? this.near = 0.0 : this.near = near;
-		far == undefined ? this.far = 0.0 : this.far = far;
-		this.viewHeight = viewHeight;
 	}
 
+	setID(id: string){
+		this.id = id;
+	}
+
+	setLabel(label: Label){
+		this.label = label;
+	}
+
+	setNear(near: number){
+		this.near = near;
+	}
+
+	setFar(far: number){
+		this.far = far;
+	}
+/*------------------------------------------------------------
+    						GETTERS
+--------------------------------------------------------------*/
 	getType(): string{
 		return this.type;
 	}
 
+	getID(){
+		return this.id;
+	}
 
+	getLabel(){
+		return this.label;
+	}
+
+	getNear(){
+		return this.near;
+	}
+
+	getFar(){
+		return this.far;
+	}
 }
+
+class OrthographicCamera extends Camera{
+	viewHeight: number;
+
+	public constructor(id: string, type: string, near: number, far: number, viewHeight: number, label: Label){
+		super()
+		if(type != undefined) this.setType(type);
+		if(id != undefined) this.setID(id);
+		if(near != undefined) this.setNear(near);
+		if(far != undefined) this.setFar(far);
+		if(label != undefined) this.setLabel(label);
+		this.viewHeight = viewHeight;
+	}
+
+/*------------------------------------------------------------
+    					GETTERS
+--------------------------------------------------------------*/
+	getViewHeight(): number{
+		return this.viewHeight;
+	}
+
+/*------------------------------------------------------------
+    					SETTERS
+--------------------------------------------------------------*/
+	setViewHeight(viewHeight: number){
+		this.viewHeight = viewHeight;
+	}
+
+} export default Camera
