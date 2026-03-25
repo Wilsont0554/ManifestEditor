c9a0b32fda4f5494ca3f782506abf5e3156edd32
Jacob Way <jacobbway1997@gmail.com>
2026-03-25 10:13:55 -0500

    Update ContainerElement.tsx

--- src/Components/ContainerElement.tsx
+++ src/Components/ContainerElement.tsx
@@ -5,6 +5,18 @@ function ContainerElement({ container, refresh }){
 
     const handleTypeChange = (e) => {
         container.setType(e.target.value);
+        if(container.getType() === 'timeline'){
+            container.SetDuration(0.0);
+            container.deleteDimensions();
+        }
+        else if(container.getType() === 'scene'){
+            container.deleteDimensions();
+            container.deleteDuration();
+        }
+        else if(container.getType() === 'canvas'){
+            container.setDimensions(0.0, 0.0);
+            container.deleteDuration();
+        }
         refresh();
     };
 
@@ -17,11 +29,14 @@ function ContainerElement({ container, refresh }){
         <div id="containerUI">
             <label>Container: {container.getType}</label>
             
-            <select name="containerType" id="containerType">
+            <select name="containerType" id="containerType" >
                 <option value="timeline" onChange={handleTypeChange}>Time Line</option>
                 <option value="scene" onChange={handleTypeChange}>Scene</option>
                 <option value="canvas" onChange={handleTypeChange}>Canvas</option>
             </select>
+
         </div>
+
+
     );
 } export default ContainerElement;
