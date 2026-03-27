e42638b582f10842fe4fb7458fc14f04ab5de1f9
Jacob Way <jacobbway1997@gmail.com>
2026-03-27 07:28:55 -0600

    Removed orthographiccamera from camera
    The code was just moved to its own file called 'OrthographicCamera'

--- src/ManifestClasses/TypeScript/SceneComponets/Camera.ts
+++ src/ManifestClasses/TypeScript/SceneComponets/Camera.ts
@@ -105,33 +105,4 @@ abstract class Camera{
 	// getInteractionMode(){
 	// 	return this.interactionMode;
 	// }
-}
-
-class OrthographicCamera extends Camera{
-	viewHeight: number;
-
-	public constructor(id: string, type: string, near: number, far: number, viewHeight: number, label: Label){
-		super()
-		if(type != undefined) this.setType(type);
-		if(id != undefined) this.setID(id);
-		if(near != undefined) this.setNear(near);
-		if(far != undefined) this.setFar(far);
-		if(label != undefined) this.setLabel(label);
-		this.viewHeight = viewHeight;
-	}
-
-/*------------------------------------------------------------
-    					GETTERS
---------------------------------------------------------------*/
-	getViewHeight(): number{
-		return this.viewHeight;
-	}
-
-/*------------------------------------------------------------
-    					SETTERS
---------------------------------------------------------------*/
-	setViewHeight(viewHeight: number){
-		this.viewHeight = viewHeight;
-	}
-
 } export default Camera
