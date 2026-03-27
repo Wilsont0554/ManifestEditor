78817a7b9096f6e86f0d0657902ad16103595b6b
Jacob Way <jacobbway1997@gmail.com>
2026-03-27 08:55:37 -0600

    Just trying to figure it out still changes

--- src/Components/CameraElement.tsx
+++ src/Components/CameraElement.tsx
@@ -1,2 +1,49 @@
+import React, { useState } from "react";
 import Camera from "../ManifestClasses/TypeScript/SceneComponets/Camera.ts";
-import React from "react";
+import OrthographicCamera from "../ManifestClasses/TypeScript/SceneComponets/OrthographicCamera.ts";
+
+function CameraElement(props: any){
+    const { camera, setcount, count } = props;
+
+    const [type, setType] = useState("DEFAULT CAMERA");
+
+    
+    function handleCameraChange(e: any){
+        if(e = "OrthographicCamera"){
+            var orthoCamera = new OrthographicCamera(0.0 , "", "OrthographicCamera", 0.0, 0.0);
+            // if(!perspectiveCamera) delete perspectiveCamera;
+        }
+    }
+
+    if(!camera) return <p>No Camera Found</p>;
+
+    const isOrthoGraphic = camera instanceof OrthographicCamera;
+    // const isPerspective = camera instanceof PerspectiveCamera;
+
+
+    if(!isOrthoGraphic /** && !isPerspective */) return <p>Camera is default Camera</p>
+
+    var cameraTypes;
+    
+    if(camera.getType().includes("Ortho")){
+        cameraTypes = "OrthographicCamera";;
+    }
+    else{
+        cameraTypes = "PerspectiveCamera";
+    }
+    
+
+    return (
+        <div className="sidebar-editor-container">
+            <div className="field-group">
+                <label>Type</label>
+                <select
+                    value={camera.getType() || ""}
+                    onChange={(e) => {handleCameraChange(e)}}>
+                        <option value={"OrthographicCamera"}>OrthographicCamera</option>
+                        <option value={"PerspectiveCamera"}>PerspectiveCamera</option>
+                </select>
+            </div>
+        </div>
+    );
+} 
