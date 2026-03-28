073d2f6cfbfbda0984bd736f6df99074d9933eee
Jacob Way <jacobbway1997@gmail.com>
2026-03-28 13:14:21 -0600

    Updated How type works in orthocamer
    It never need a choice so it just defaults to OrthographicCamera now when its created.

--- src/ManifestClasses/TypeScript/SceneComponets/OrthographicCamera.ts
+++ src/ManifestClasses/TypeScript/SceneComponets/OrthographicCamera.ts
@@ -4,10 +4,10 @@ import Camera from './Camera'
 class OrthographicCamera extends Camera{
 	viewHeight: number;
 
-	public constructor(viewHeight: number, id?: string, type?: string, near?: number, far?: number, label?: Label){
+	public constructor(viewHeight: number, id?: string,  near?: number, far?: number, label?: Label){
 		super()
-		type != undefined ? this.setType(type) : undefined;
-		id != undefined ? this.setID(id) : "https://iiif/camera/default.com";
+		this.type = "OrthographicCamera";
+		id != undefined ? this.setID(id) : id = "https://iiif/camera/default.com";
 		near != undefined ? this.setNear(near) : 0.0;
 		far != undefined ? this.setFar(far) : 0.0;
 		label != undefined ? this.setLabel(label) : "https://iiif/labelman.com";
