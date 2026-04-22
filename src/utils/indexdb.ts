1687436a00f3c7de33d5a0b5e05d7251aba06da0
Cao <nguyens3204@my.uwstout.edu>
2026-04-22 11:12:52 -0500

    add comments to the indexedDB code

--- src/utils/indexdb.ts
+++ src/utils/indexdb.ts
@@ -1,6 +1,12 @@
 import ManifestObject from "@/ManifestClasses/ManifestObject";
 import { IiifManifest } from "@/types/iiif";
 
+/**
+ * Utility class for handling IndexedDB operations related to saving and retrieving IIIF manifest data.
+ * We use IndexedDB to persist the latest manifest data locally in the user's browser.
+ * The class provides methods to open the database, save a manifest object, and retrieve a manifest object by ID.
+ * The manifest data is stored in an object store named "projects" with a key of "current" for the latest manifest.
+ */
 export class IndexedDB {
   dbName: string = "IIIfManifest3DProjects";
   version: number = 1;
@@ -12,6 +18,11 @@ export class IndexedDB {
     if (version) this.version = version;
     IndexedDB.request = indexedDB.open(this.dbName, this.version);
   }
+
+  /**
+   * Opens the IndexedDB database.
+   * @returns A promise resolving to the database instance.
+   */
 
   open(): Promise<IDBDatabase> {
     if (IndexedDB.db) {
@@ -27,17 +38,24 @@ export class IndexedDB {
         }
       };
 
+      //handle successful opening of the database
       IndexedDB.request.onsuccess = (event) => {
         IndexedDB.db = (event.target as IDBOpenDBRequest).result;
         resolve(IndexedDB.db);
       };
 
+      //handle errors during opening of the database
       IndexedDB.request.onerror = (event) => {
         reject((event.target as IDBOpenDBRequest).error);
       };
     });
   }
 
+  /**
+   * Saves a manifest object to the IndexedDB under the "current" key in the "projects" object store.
+   * @param manifest the manifest object you want to store to indexDB
+   * @returns A promise resolving to the saved manifest object
+   */
   saveProject(manifest: object) {
     return new Promise((resolve, reject) => {
       if (!IndexedDB.db) {
@@ -63,6 +81,11 @@ export class IndexedDB {
     });
   }
 
+  /**
+   * Retrieves a manifest object from the IndexedDB by its ID in the "projects" object store.
+   * @param id the ID of the manifest object to retrieve
+   * @returns A promise resolving to the retrieved manifest object
+   */
   getProject(id: string) {
     return new Promise<object>((resolve, reject) => {
       if (!IndexedDB.db) {
